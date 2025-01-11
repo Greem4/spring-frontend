5666859b95c0978a9013fcc4d89e61d78acebaff
Greem4 <i@greem4.ru>
2025-01-11 17:13:43 +0300

    подготовка

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './AuthContext';  // Импортируем AuthProvider
+import { AuthProvider } from './AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
